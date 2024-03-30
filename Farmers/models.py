dad173b269a982ce71d452d3bfea64dffbdf4596
aderawmolla <mollaaderaw@gmail.com>
2024-03-30 22:32:02 +0300

    [FEATURE]user and farmer model

--- Farmers/models.py
+++ Farmers/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from Users.models import CustomUser
+
 # Create your models here.
+class Farmer(models.Model):
+    full_name = models.CharField(max_length=255)
+    phone_no = models.CharField(max_length=20)
+    location_latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    location_longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    location_name = models.CharField(max_length=255)
+    profile_image = models.CharField(max_length=255)
+    agent_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.full_name
--- Orders/admin.py
+++ Orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from Farmers.models import Farmer
+from Users.models import CustomUser
+
 # Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Farmer)
