67e31d1fc3667f208bbcca4ded3de3e48ec14d95
lijsamuael <lijsamuael@gmail.com>
2024-04-10 03:09:08 -0400

    (feat) products api implemented

--- Products/urls.py
+++ Products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-import crud as crudApi
+import Products.views as farmerViews
+
 urlpatterns = [
-    path('',crudApi.retrieve_object_by_pk),
-    path('insert_objects_one/',crudApi.insert_objects_one),
-    path('retrieve_object_by_pk/<int:pk>/',crudApi.retrieve_object_by_pk),
-    path('delete_users_by_pk/<int:pk>/',crudApi.delete_object_by_pk),
-    path('update_users_by_pk/<int:pk>/',crudApi.update_object_by_pk),
+    path('', farmerViews.products, name='products'),
+    path('products_by_pk/<int:pk>/', farmerViews.products_by_pk, name='product_by_pk'),
+    path('insert_product_one/', farmerViews.insert_product_one, name='insert_product_one'),
+    path('delete_product_by_pk/<int:pk>/', farmerViews.delete_product_by_pk, name='delete_product_by_pk'),
+    path('update_product_by_pk/<int:pk>/', farmerViews.update_product_by_pk, name='update_product_by_pk'),
+    path('insert_multiple_products/', farmerViews.insert_multiple_products, name='insert_multiple_products'),
 ]
---
+++ Products/views.py
@@ -0,0 +1,74 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from Products.models import Product
+from Products.serializers import ProductSerializer
+
+@api_view(['GET'])
+def products_by_pk(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def products(request):
+    try:
+        products = Product.objects.all()
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def insert_product_one(request):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def delete_product_by_pk(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_product_by_pk(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = ProductSerializer(product, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def insert_multiple_products(request):
+    serializer = ProductSerializer(data=request.data, many=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
